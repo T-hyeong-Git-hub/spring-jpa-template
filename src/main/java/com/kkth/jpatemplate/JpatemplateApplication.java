d9ecf2f07c3b5811f788d4129ce1ef27c800ca8e
taehyeong <kkth0705@naver.com>
2026-03-10 00:11:43 +0900

    Initial Spring Boot JPA Template project

---
+++ src/main/java/com/kkth/jpatemplate/JpatemplateApplication.java
@@ -0,0 +1,13 @@
+package com.kkth.jpatemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpatemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpatemplateApplication.class, args);
+	}
+
+}
